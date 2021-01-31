a33cb9fcf1cdc9cc854252d4eb66bd7926d4250c
BarokDG <it.barok.dagim@gmail.com>
2021-01-31 20:03:51 +0300

    Sort by date added

--- Task Manager App/assets/js/app.js
+++ Task Manager App/assets/js/app.js
@@ -6,6 +6,8 @@ const filter = document.querySelector("#filter");
 const taskList = document.querySelector(".collection");
 const clearBtn = document.querySelector(".clear-tasks");
 const reloadIcon = document.querySelector(".fa");
+const Ascending = document.querySelector('#asc');
+const Descending = document.querySelector('#desc');
 
 // Events
 
@@ -14,6 +16,8 @@ clearBtn.addEventListener("click", clearAllTasks);
 filter.addEventListener("keyup", filterTasks);
 taskList.addEventListener('click', removeTask);
 reloadIcon.addEventListener('click', reloadPage);
+Ascending.addEventListener('click', sortAsc);
+Descending.addEventListener('click', sortDesc);
 // Event Handlers
 
 function addNewTask(e) {
@@ -41,6 +45,35 @@ function addNewTask(e) {
     link.innerHTML = '<i class="fa fa-remove"></i>';
     // Append link to li
     li.appendChild(link);
+
+    // Add date
+
+    const divDate = document.createElement("div");
+    divDate.className = "div-date";
+    divDate.style.display = "block";
+
+    let today = new Date();
+
+    const dateText = document.createTextNode(
+        today.getFullYear() +
+        "-" +
+        today.getMonth() +
+        "-" +
+        today.getDate() +
+        " " +
+        today.getHours() +
+        ":" +
+        today.getMinutes() +
+        ":" +
+        today.getSeconds() +
+        ":" +
+        today.getMilliseconds()
+    );
+
+    divDate.appendChild(dateText);
+
+    li.appendChild(divDate);
+
     // Append to ul 
     taskList.appendChild(li);
 
@@ -67,13 +100,62 @@ function filterTasks(e) {
 
     let filterSearch = filter.value;
     let listItems = document.querySelectorAll(".collection-item");
-    
+
     listItems.forEach((item) => {
-        if (item.textContent.indexOf(filterSearch)){
+        if (item.textContent.indexOf(filterSearch)) {
             item.style.display = "none";
-        }
-        else{
+        } else {
             item.style.display = "block";
         }
     })
+}
+
+function descSort(arr) {
+    var len = arr.length;
+    for (var i = len - 1; i >= 0; i--) {
+        for (var j = 1; j <= i; j++) {
+            if (arr[j - 1].innerText < arr[j].innerText) {
+                var temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+function sortDesc() {
+    const divDate = document.querySelectorAll(".div-date");
+    const taskDates = Array.from(divDate);
+    const dates = descSort(taskDates);
+    taskList.innerHTML = "";
+    dates.forEach((date) => {
+        taskList.appendChild(date.parentElement);
+    });
+}
+
+
+
+function sortAsc() {
+    const divDate = document.querySelectorAll(".div-date");
+    const taskDates = Array.from(divDate);
+    const dates = ascSort(taskDates);
+    taskList.innerHTML = ""
+    dates.forEach((date) => {
+        taskList.appendChild(date.parentElement);
+    });
+}
+
+function ascSort(arr) {
+    var len = arr.length;
+    for (var i = len - 1; i >= 0; i--) {
+        for (var j = 1; j <= i; j++) {
+            if (arr[j - 1].innerText > arr[j].innerText) {
+                var temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
 }
